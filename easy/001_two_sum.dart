df2b0b859865465906bbdc816064c3026bf539b4
eghtyd <iryna.kashliak@final.edu.tr>
2026-03-15 23:51:16 +0200

    001_two_sum.dart

--- easy/001_two_sum.dart
+++ easy/001_two_sum.dart
@@ -1,15 +1,17 @@
-List<int> twoSum(List<int> nums, int target) {
-  Map<int, int> map = {};
+class Solution {
+  List<int> twoSum(List<int> nums, int target) {
+    Map<int, int> seen = {};
 
-  for (int i = 0; i < nums.length; i++) {
-    int complement = target - nums[i];
+    for (int i = 0; i < nums.length; i++) {
+      int complement = target - nums[i];
 
-    if (map.containsKey(complement)) {
-      return [map[complement]!, i];
+      if (seen.containsKey(complement)) {
+        return [seen[complement]!, i];
+      }
+
+      seen[nums[i]] = i;
     }
 
-    map[nums[i]] = i;
+    return [];
   }
-
-  return [];
 }
