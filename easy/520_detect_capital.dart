c7a36ba0e690fa2977aaa9a8e38e9a35700c206a
eghtyd <iryna.kashliak@final.edu.tr>
2026-03-16 08:43:52 +0200

    520_detect_capital.dart

---
+++ easy/520_detect_capital.dart
@@ -0,0 +1,8 @@
+class Solution {
+  bool detectCapitalUse(String word) {
+    return word == word.toUpperCase() ||
+        word == word.toLowerCase() ||
+        (word[0] == word[0].toUpperCase() &&
+            word.substring(1) == word.substring(1).toLowerCase());
+  }
+}
